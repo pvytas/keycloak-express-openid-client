60d7404fb3f69162702833688a440ee25f2a2120
pvytas <pvytas@bitnobi.com>
2025-03-03 14:22:53 -0500

    wip

--- keycloak-http/utils/disable_https.sh
+++ keycloak-http/utils/disable_https.sh
@@ -1,4 +1,4 @@
 cd /opt/keycloak/bin
 ./kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin
-./kcadm.sh update realms/master -s sslRequired=NONE'
+./kcadm.sh update realms/master -s sslRequired=NONE
 
