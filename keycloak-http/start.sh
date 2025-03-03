32b50b01f6d87e03beb546ba1a641a1197f09509
pvytas <pvytas@bitnobi.com>
2025-03-03 12:45:25 -0500

    xx

--- keycloak-http/start.sh
+++ keycloak-http/start.sh
@@ -6,5 +6,5 @@ else
     docker-compose  up -d
 fi
 
-sleep 3
+sleep 1
 docker exec -it keycloak /bin/bash -c "cd /opt/keycloak/bin;./kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin;./kcadm.sh update realms/master -s sslRequired=NONE"
