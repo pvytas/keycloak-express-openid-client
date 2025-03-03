d10ba74636776fdd8c0183a89287ffe386ad2a71
pvytas <pvytas@bitnobi.com>
2025-03-03 14:17:56 -0500

    wip

--- keycloak-http/start.sh
+++ keycloak-http/start.sh
@@ -8,3 +8,5 @@ fi
 
 # sleep 1
 # docker exec -it keycloak /bin/bash -c 'cd /opt/keycloak/bin; ./kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin; ./kcadm.sh update realms/master -s sslRequired=NONE'
+sleep 5
+docker exec -it keycloak /opt/keycloak/utils/disable_https.sh
